96892519f1333889d67c0c2fef929cedfb974068
Emwaii <m.yulianto17@gmail.com>
2023-05-28 10:53:57 +0700

    first

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+/*=============== SHOW HIDDEN - PASSWORD ===============*/
+let docTitle = document.title;
+window.addEventListener("blur", () => {
+   document.title = "Come Back :(";
+});
+
+window.addEventListener("focus", ()=> {
+   document.title = docTitle;
+});
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.onkeydown = function(e) {
+      if(event.keyCode == 123) {
+         return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+         return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+         return false;
+      }
+      if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+         return false;
+      }
+      if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+         return false;
+      }
+      if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+         return false;
+      }
+}
