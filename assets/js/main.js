4508285ab9ac4293a1bd1ef5f6d0b0ba525d284d
Emwaii <m.yulianto17@gmail.com>
2023-05-28 11:36:29 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,25 +7,31 @@ window.addEventListener("blur", () => {
 window.addEventListener("focus", ()=> {
    document.title = docTitle;
 });
+window.addEventListener('load', function() {
+   // Hide the preloader once the webpage is fully loaded
+   var preloader = document.getElementById('preloader');
+   preloader.style.display = 'none';
+   document.body.classList.remove('disable-scrolling');
 
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.onkeydown = function(e) {
-      if(event.keyCode == 123) {
-         return false;
-      }
-      if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-         return false;
-      }
-      if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-         return false;
-      }
-      if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-         return false;
-      }
-      if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-         return false;
-      }
-      if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
-         return false;
-      }
-}
+});
+// document.addEventListener('contextmenu', event => event.preventDefault());
+// document.onkeydown = function(e) {
+//       if(event.keyCode == 123) {
+//          return false;
+//       }
+//       if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+//          return false;
+//       }
+//       if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+//          return false;
+//       }
+//       if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+//          return false;
+//       }
+//       if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//          return false;
+//       }
+//       if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)) {
+//          return false;
+//       }
+// }
